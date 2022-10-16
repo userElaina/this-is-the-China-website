4fed2c0ac7e43c79c8af9ec608fd0408f33093dc
userelaina <userelaina@pm.me>
2022-10-16 11:24:20 +0000

    :sparkles: change img src from baidu to github

--- wikipedia/wikipedia2baidu.js
+++ wikipedia/wikipedia2baidu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 百度百科美化
 // @namespace userElaina
-// @version 2022.10.15.1
+// @version 2022.10.16.20
 // @description 中国人就用百度百科
 // @author userElaina
 // @license MIT
@@ -10,10 +10,10 @@
 // ==/UserScript==
 
 (function () {
-    document.querySelector('link[rel="icon"]').href = 'https://www.baidu.com/favicon.ico';
-    document.title = document.title.replace(/\s-[\s\S]*/g, "_百度百科");
+    document.querySelector('link[rel="icon"]').href = 'https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/wikipedia/baidu.ico';
+    document.title = document.title.replace(/\s-[\s\S]*/g, " - 百度百科");
     var bigLogo = document.querySelector('a[class="mw-wiki-logo"]');
-    bigLogo.innerHTML = '<img src="https://www.baidu.com/img/bd_logo1.png" alt="Baidu" data-atf="3" height="100px" width="160px">';
+    bigLogo.innerHTML = '<img src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/wikipedia/baidu_big.png" style="padding:10px;padding-top:40px;width:-webkit-fill-available;">';
     bigLogo.className = '';
     document.getElementById("siteSub").innerText = '百度百科或许会倒闭, 但一定不会变质!'
 })();
