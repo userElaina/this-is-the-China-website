f3f4638f7d54a6a4774cde8a6a1f7e49d708d58b
userelaina <userelaina@pm.me>
2022-10-16 10:01:12 +0000

    :lipstick: change & add imgs

--- wikipedia/wikipedia2baidu.js
+++ wikipedia/wikipedia2baidu.js
@@ -13,7 +13,7 @@
     document.querySelector('link[rel="icon"]').href = 'https://www.baidu.com/favicon.ico';
     document.title = document.title.replace(/\s-[\s\S]*/g, "_百度百科");
     var bigLogo = document.querySelector('a[class="mw-wiki-logo"]');
-    bigLogo.innerHTML = '<img src="https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png" alt="Baidu" data-atf="3" height="100px" width="160px">';
+    bigLogo.innerHTML = '<img src="https://www.baidu.com/img/bd_logo1.png" alt="Baidu" data-atf="3" height="100px" width="160px">';
     bigLogo.className = '';
     document.getElementById("siteSub").innerText = '百度百科或许会倒闭, 但一定不会变质!'
 })();
