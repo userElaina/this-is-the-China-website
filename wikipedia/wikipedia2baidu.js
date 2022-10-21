088dcbbef896f3d1699bf6cb3ed86b824d58d29b
Elaina <userelaina@pm.me>
2022-10-21 05:35:05 +0000

    :sparkles: yt sub button & progress

--- wikipedia/wikipedia2baidu.js
+++ wikipedia/wikipedia2baidu.js
@@ -12,7 +12,7 @@
 (function () {
     document.querySelector('link[rel="icon"]').href = 'https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/wikipedia/baidu.ico';
     document.title = document.title.replace(/\s-[\s\S]*/g, " - 百度百科");
-    var bigLogo = document.querySelector('a[class="mw-wiki-logo"]');
+    var bigLogo = document.querySelector('a.mw-wiki-logo');
     bigLogo.innerHTML = '<img src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/wikipedia/baidu_big.png" style="padding:10px;padding-top:40px;width:-webkit-fill-available;">';
     bigLogo.className = '';
     document.getElementById("siteSub").innerText = '百度百科或许会倒闭, 但一定不会变质!'
