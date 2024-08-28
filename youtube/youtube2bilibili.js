f4b4f3b5e7020e8926d911c6323bf0a4a292246e
userElaina <userelaina@pm.me>
2024-08-28 12:52:20 +0000

    :bookmark: ytb2bili: 2024.08.28.02

--- youtube/youtube2bilibili.js
+++ youtube/youtube2bilibili.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 哔哩哔哩美化
 // @namespace https://github.com/userElaina/this-is-the-China-website
-// @version 2024.08.28.01
+// @version 2024.08.28.02
 // @description 中国人就用哔哩哔哩
 // @author userElaina
 // @license MIT
