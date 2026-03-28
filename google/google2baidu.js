1f3d953cfbe3077a8a861e7f8dae0350adcecf6c
userElaina <userelaina@pm.me>
2026-03-28 09:52:41 +0000

    :arrow_up: :bookmark: google

--- google/google2baidu.js
+++ google/google2baidu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 百度搜索美化
 // @namespace https://github.com/userElaina/this-is-the-China-website
-// @version 2024.08.28.01
+// @version 2026.03.28.01
 // @description 中国人就用百度搜索
 // @author userElaina somereason
 // @license MIT
