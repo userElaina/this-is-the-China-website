e6326a7ac5c8e1e0166b816ce3098d501571f618
userElaina <userelaina@pm.me>
2026-04-01 09:32:03 +0000

    :sparkles: gg2w

--- google2what/google2what.js
+++ google2what/google2what.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name 百度搜索美化
+// @name 超级AI搜索
 // @namespace https://github.com/userElaina/this-is-the-China-website
 // @version 2026.04.01.01
-// @description 中国人就用百度搜索
+// @description 最强国产超级AI搜索
 // @author userElaina somereason
 // @license MIT
 // @match *://*.google.com/
@@ -55,61 +55,8 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = 'https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu.ico';
+    link.href = 'https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google2what/what.ico';
     document.getElementsByTagName('head')[0].appendChild(link);
-
-    // change search style
-    // f_succ(() => {
-    //     let RNNXgb = document.getElementsByClassName('RNNXgb');
-    //     if (RNNXgb.length <= 0) {
-    //         RNNXgb = document.getElementsByClassName('o6juZc');
-    //     }
-    //     if (RNNXgb.length <= 0) {
-    //         return false;
-    //     }
-    //     let searchStyle = RNNXgb[0].style;
-    //     searchStyle.boxShadow = "0 0 0 0";
-    //     searchStyle.border = 0;
-    //     searchStyle.borderRadius = 0;
-    //     searchStyle.background = '#4e6ef21f';
-    //     return true;
-    // });
-
-    // func: change search button
-    async function SearchButton(s0) {
-        await f_succ(() => {
-            let Tg7LZd = document.getElementsByClassName('Tg7LZd');
-            if (Tg7LZd.length <= 0) {
-                Tg7LZd = document.getElementsByClassName('rCGXm');
-            }
-            if (Tg7LZd.length <= 0) {
-                return false;
-            }
-            let height = Tg7LZd[0].clientHeight;
-            Tg7LZd[0].innerHTML = '<img height=' + height + ' src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/' + s0 + '.png">';
-            return true;
-        });
-    }
-
-    // func: change big logo
-    // async function BigLogo(str) {
-    //     await f_succ(() => {
-    //         let bigLogo = document.querySelector("[alt='" + str + "']");
-    //         if (bigLogo === null) {
-    //             return false;
-    //         }
-    //         bigLogo.src = "https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_big.png";
-    //         bigLogo.removeAttribute("srcset");
-    //         bigLogo.width = 117 * 2;
-    //         bigLogo.height = 38 * 2;
-    //         /*
-    //         let paddingTop = bigLogo.style.paddingTop.replace("px", "");
-    //         let paddingTopInt = parseInt(paddingTop);
-    //         bigLogo.style.paddingTop = (paddingTopInt - 20) + "px";
-    //         */
-    //         return true;
-    //     });
-    // }
 
     if (window.location.href.indexOf("/search") > -1) {
         // search page
@@ -118,6 +65,8 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
         f_succ(() => {
             let logo = document.getElementById("logo");
             let logoArr;
+            let img_src = "https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google2what/what_small.png";
+            let img_str = '<img src="' + img_src + '" style="background:none" width="96" height="32" data-atf="1" data-frt="0"></img>';
             if (logo === null) {
                 logoArr = document.getElementsByClassName("logo");
                 if (logoArr.length <= 0) {
@@ -140,38 +89,28 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
                     if (logo.childElementCount <= 0) {
                         return false;
                     }
-                    logo.innerHTML = '<img src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_small.png" style="background:none" width="117" height="38" data-atf="1" data-frt="0"></img>';
+                    logo.innerHTML = img_str;
                 } else {
-                    logo.innerHTML = '<img src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_small.png" style="background:none" width="117" height="38" data-atf="1" data-frt="0"></img>';
+                    logo.innerHTML = img_str;
                 }
             } else {
-                img.src = 'https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_small.png';
+                img.src = img_src;
             }
             return true;
         });
 
-        document.title = document.title.replace(/\s-[\s\S]*/g, " - 百度搜索");
-        SearchButton('search');
+        document.title = document.title.replace(/\s-[\s\S]*/g, " - 超级AI搜索");
 
-        let naviImageUrl = "https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/icons.png";
-        let navTabSpans = document.getElementsByClassName("SJajHc");
-        for (let i = 0; i < navTabSpans.length; i++) {
-            navTabSpans[i].style.width = "22px";
-            if (i === 0) {
-                navTabSpans[i].style.background = 'url("' + naviImageUrl + '") no-repeat 0px 0px';
-            } else if (i == navTabSpans.length - 1) {
-                navTabSpans[i].style.background = 'url("' + naviImageUrl + '") no-repeat 0px 0px';
-            } else if (navTabSpans[i].classList.contains("NVbCr")) {
-                navTabSpans[i].style.background = i % 2 == 1 ? 'url("' + naviImageUrl + '") no-repeat -144px -288px' : 'url("' + naviImageUrl + '") no-repeat -144px -282px';
-            } else {
-                navTabSpans[i].style.background = 'url("' + naviImageUrl + '") no-repeat -96px -288px';
-            }
-        }
+        document.querySelectorAll("span.SJajHc").forEach(v => {
+            let parent = v.parentElement;
+            v.remove();
+            parent.innerHTML = '&nbsp;&nbsp;' + parent.innerHTML + '&nbsp;&nbsp;';
+        });
 
     } else if (window.location.href.indexOf("scholar") > -1) {
 
         if (window.location.href.indexOf("/scholar?") > -1) {
-            document.title = document.title.replace(/\s-[\s\S]*/g, " - 百度学术");
+            document.title = document.title.replace(/\s-[\s\S]*/g, " - 超级AI学术搜索");
             f_succ(() => {
                 let gs_hdr_lgo = document.getElementById("gs_hdr_lgo");
                 if (gs_hdr_lgo === null) {
@@ -181,13 +120,13 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
                 return true;
             });
         } else {
-            document.title = "百度学术 - 保持学习的态度";
+            document.title = "最强国产超级AI学术搜索";
             f_succ(() => {
                 let gs_hdr_hp_lgo = document.getElementById("gs_hdr_hp_lgo");
                 if (gs_hdr_hp_lgo === null) {
                     return false;
                 }
-                gs_hdr_hp_lgo.srcset = "https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/scholar.png";
+                gs_hdr_hp_lgo.srcset = "https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google2what/what_small.png";
                 return true;
             });
         }
@@ -201,7 +140,7 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
             if (bigLogo === null) {
                 return false;
             }
-            bigLogo.innerHTML = '<style>.rSk4se{max-height:92px;position:relative}.lnXdpd{max-height:100%;max-width:100%;object-fit:contain;object-position:center bottom;width:auto}</style><img class="lnXdpd" alt="Google Images" src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_img.webp" width="254" height="80">';
+            bigLogo.innerHTML = '<style>.rSk4se{max-height:92px;position:relative}.lnXdpd{max-height:100%;max-width:100%;object-fit:contain;object-position:center bottom;width:auto}</style><img class="lnXdpd" alt="Google Images" src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google2what/what_small.png" width="256" height="85">';
             return true;
         });
 
@@ -215,18 +154,18 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
             return true;
         });
 
-        document.title = "百度图片, 发现多彩世界";
+        document.title = "最强国产超级AI识图";
 
         document.querySelectorAll('span.gb_Id').forEach(v => {
-            v.innerHTML = '登录';
+            v.innerHTML = '登陆';
         });
 
         document.querySelectorAll("a.pHiOh, div.ayzqOc.pHiOh").forEach(v => {
             // v.innerHTML = v.innerHTML.replace(/\s?Google\s?/, "百度");
-            v.innerHTML = v.innerHTML.replace("About", "关于百度");
+            v.innerHTML = v.innerHTML.replace("About", "关于超级AI搜索");
             v.innerHTML = v.innerHTML.replace("Advertising", "投放广告");
             v.innerHTML = v.innerHTML.replace("Business", "商务合作");
-            v.innerHTML = v.innerHTML.replace("How Search works", "搜索运作原理");
+            v.innerHTML = v.innerHTML.replace("How Search works", "运作原理");
             v.innerHTML = v.innerHTML.replace("Privacy", "隐私");
             v.innerHTML = v.innerHTML.replace("Terms", "条款");
             v.innerHTML = v.innerHTML.replace("Settings", "设置");
@@ -238,7 +177,7 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
                 return false;
             }
             btnK.forEach(v => {
-                v.value = "百度搜索";
+                v.value = "超级AI搜索";
             });
             return true;
         });
@@ -259,7 +198,7 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
             if (footnote === null) {
                 return false;
             }
-            footnote.innerHTML = '百度提供: ' + footnote.innerHTML.slice(footnote.innerHTML.indexOf('<'));
+            footnote.innerHTML = '超级AI搜索提供: ' + footnote.innerHTML.slice(footnote.innerHTML.indexOf('<'));
             return true;
         });
 
@@ -279,20 +218,20 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
                 return false;
             }
             // bigLogo.removeAttribute("srcset");
-            bigLogo.innerHTML = '<style>.rSk4se{max-height:92px;position:relative}.lnXdpd{max-height:100%;max-width:100%;object-fit:contain;object-position:center bottom;width:auto}</style><img class="lnXdpd"  aria-label="Google" role="img" src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google/baidu_big.png" width="180" height="86">';
+            bigLogo.innerHTML = '<style>.rSk4se{max-height:92px;position:relative}.lnXdpd{max-height:100%;max-width:100%;object-fit:contain;object-position:center bottom;width:auto}</style><img class="lnXdpd"  aria-label="Google" role="img" src="https://raw.githubusercontent.com/userElaina/this-is-the-China-website/main/google2what/what_small.png" width="256" height="85">';
             return true;
         });
 
-        document.title = "百度一下, 你就知道";
+        document.title = "最强国产超级AI搜索";
 
         document.querySelectorAll('a.gb_d, a.gb_p, a.gb_q, a.gb_F, a.gb_H, a.gb_Z').forEach(v => {
             if (v.dataset.pid === '2') {
-                v.innerText = '百度识图';
+                v.innerText = '超级AI识图';
                 if (v.href.indexOf("google.cn") > -1) {
                     v.href = 'https://images.google.com/imghp';
                 }
             } else if (v.dataset.pid === '23') {
-                v.innerHTML = '百度邮箱';
+                v.innerHTML = '超级AI邮箱';
             }
         });
 
@@ -306,10 +245,10 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
 
         document.querySelectorAll("a.pHiOh, div.ayzqOc.pHiOh").forEach(v => {
             // v.innerHTML = v.innerHTML.replace(/\s?Google\s?/, "百度");
-            v.innerHTML = v.innerHTML.replace("About", "关于百度");
+            v.innerHTML = v.innerHTML.replace("About", "关于超级AI搜索");
             v.innerHTML = v.innerHTML.replace("Advertising", "投放广告");
             v.innerHTML = v.innerHTML.replace("Business", "商务合作");
-            v.innerHTML = v.innerHTML.replace("How Search works", "搜索运作原理");
+            v.innerHTML = v.innerHTML.replace("How Search works", "运作原理");
             v.innerHTML = v.innerHTML.replace("Privacy", "隐私");
             v.innerHTML = v.innerHTML.replace("Terms", "条款");
             v.innerHTML = v.innerHTML.replace("Settings", "设置");
@@ -321,7 +260,7 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
                 return false;
             }
             btnK.forEach(v => {
-                v.value = "百度搜索";
+                v.value = "超级AI搜索";
             });
             return true;
         });
@@ -342,7 +281,7 @@ async function f_succ(f, msSleep = 500, maxCount = 10) {
             if (footnote === null) {
                 return false;
             }
-            footnote.innerHTML = '百度提供: ' + footnote.innerHTML.slice(footnote.innerHTML.indexOf('<'));
+            footnote.innerHTML = '超级AI搜索提供: ' + footnote.innerHTML.slice(footnote.innerHTML.indexOf('<'));
             return true;
         });
 
